f6b5cafaf75c3b1ea1c7727962c660e95bc72503
yshrsmz <the.phantom.bane@gmail.com>
2021-02-21 19:47:57 +0900

    add new-milestone package

---
+++ .eslintrc.js
@@ -0,0 +1,15 @@
+/* eslint-env node */
+module.exports = {
+  root: true,
+  parser: '@typescript-eslint/parser',
+  plugins: ['@typescript-eslint', 'prettier'],
+  extends: [
+    'eslint:recommended',
+    'plugin:@typescript-eslint/recommended',
+    'prettier',
+    'prettier/@typescript-eslint',
+  ],
+  rules: {
+    'prettier/prettier': 'error',
+  },
+}
---
+++ packages/new-milestone/index.ts
@@ -0,0 +1,56 @@
+import { Octokit } from '@octokit/rest'
+import { parseISO, addDays, formatISO } from 'date-fns'
+
+const OWNER = 'AndroidDagashi'
+const REPO = 'AndroidDagashi'
+
+async function main(): Promise<string> {
+  const octokit = new Octokit({
+    auth: process.env.PUSH_ACCESS_TOKEN,
+    timeZone: 'Asiz/Tokyo',
+  })
+
+  const milestones = await octokit.issues.listMilestones({
+    owner: OWNER,
+    repo: REPO,
+    state: 'all',
+    sort: 'due_on',
+    direction: 'desc',
+    per_page: 2,
+    page: 1,
+  })
+
+  console.log(milestones)
+
+  const latest = milestones.data[0]
+  console.log('latest milestone:', latest)
+
+  if (latest.state === 'open') {
+    // milestone with 'open' state exists. nothing to do.
+    return `open milestone(${latest.number}) still exists.`
+  }
+  // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
+  const dueOn = parseISO(latest.due_on!)
+
+  const nextMilestoneNumber = latest.number + 1
+  const nextDueOn = addDays(dueOn, 7)
+
+  const nextTitle = `${nextMilestoneNumber} ${formatISO(nextDueOn, {
+    representation: 'date',
+  })}`
+
+  const result = await octokit.issues.createMilestone({
+    owner: OWNER,
+    repo: REPO,
+    title: nextTitle,
+    due_on: formatISO(nextDueOn),
+  })
+
+  console.log('result:', result.data)
+
+  return `${result.data.number}, ${result.data.title}, ${result.data.due_on}`
+}
+
+main()
+  .then((result) => console.log('finished', result))
+  .catch((e) => console.error(e))
